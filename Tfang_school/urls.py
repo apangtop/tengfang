2009346df7c87a007385c48247fa7d3cba94874f
apangtop <771154265@qq.com>
2025-03-17 16:06:37 +0800

    添加朝会思政节目

--- Tfang_school/urls.py
+++ Tfang_school/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from broadcast.views import index, video_player
+from broadcast.views import index, video_player, video_player2
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('api/', include('broadcast.urls.broadcast_urls')),
     path('video/', video_player, name='video_player'),
+    path('video2/', video_player2, name='video_player2'),
 ]
 
