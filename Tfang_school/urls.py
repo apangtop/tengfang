c6aff5473fb428200a0ed1662b8fbe2016d11332
apangtop <771154265@qq.com>
2025-06-03 22:15:24 +0800

    1

--- Tfang_school/urls.py
+++ Tfang_school/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from broadcast.views import index, video_player, video_player2
 
@@ -9,5 +11,6 @@ urlpatterns = [
     path('api/', include('broadcast.urls.broadcast_urls')),
     path('video/', video_player, name='video_player'),
     path('video2/', video_player2, name='video_player2'),
-]
+    path('qrcode_upload/', include('qrcode_upload.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
