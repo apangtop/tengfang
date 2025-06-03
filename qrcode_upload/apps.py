0860e80f0d729135c04048dd8e416d76614c206e
apangtop <771154265@qq.com>
2025-06-03 22:19:52 +0800

    1

--- qrcode_upload/apps.py
+++ qrcode_upload/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class QrcodeUploadConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'qrcode_upload'
+    verbose_name = '二维码上传'
