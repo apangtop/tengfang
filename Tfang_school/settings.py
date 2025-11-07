efaf33fc281a99aa34733cc4d77ff754eac62a2e
apangtop <771154265@qq.com>
2025-11-07 16:54:07 +0800

    修改朝会思政地址

--- Tfang_school/settings.py
+++ Tfang_school/settings.py
@@ -10,7 +10,7 @@ OSS_ACCESS_KEY_SECRET = 'REMOVED_OSS_ACCESS_KEY_SECRET'
 OSS_BUCKET_NAME = 'cqbz-indoor-exercises'
 OSS_ENDPOINT = 'oss-cn-chengdu.aliyuncs.com'
 OSS_VIDEO_PATH = '7da4f66416cb4507e0ba34fac906a0df.mp4'
-OSS_VIDEO_PATH_two = '0519朝会最终版.mp4'
+OSS_VIDEO_PATH_two = '11月7日.mp4'
 # 构建路径
 BASE_DIR = Path(__file__).resolve().parent.parent
 
